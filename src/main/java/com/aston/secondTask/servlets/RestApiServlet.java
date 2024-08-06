6d3cebd59a797da37c1a0590819cd4510382e7cd
Sofya Aheyeva <zozolulu1894@gmail.com>
2024-08-06 18:58:38 +0300

    testy 80 -12 commit

--- src/main/java/com/aston/secondTask/servlets/RestApiServlet.java
+++ src/main/java/com/aston/secondTask/servlets/RestApiServlet.java
@@ -16,21 +16,21 @@ import java.sql.SQLException;
 @WebServlet(urlPatterns = "/rest/*")
 public class RestApiServlet extends HttpServlet {
 
-    private static final String COORDINATOR_NOT_FOUND = "Sorry, coordinator hasn't found";
-    private static final String STUDENT_NOT_FOUND = "Sorry, student hasn't found";
-    private static final String COORDINATORS_NOT_FOUND = "Sorry, coordinators hasn't found";
-    private static final String COURSES_NOT_FOUND = "Sorry, courses hasn't found";
-    private static final String COORDINATOR_NOT_CREATED = "Sorry, coordinator hasn't created";
-    private static final String COURSE_NOT_CREATED = "Sorry, course hasn't created";
-    private static final String STUDENT_NOT_CREATED = "Sorry, student hasn't created";
-    private static final String STUDENT_COURSE_NOT_CREATED = "Sorry, student course hasn't created";
-    private static final String COORDINATOR_NOT_DELETED = "Sorry, coordinator hasn't deleted";
-    private static final String COURSE_NOT_DELETED = "Sorry, course hasn't deleted";
-    private static final String STUDENT_NOT_DELETED = "Sorry, student hasn't deleted";
-    private static final String STUDENT_COURSE_NOT_DELETED = "Sorry, student's course hasn't deleted";
-    private static final String COORDINATOR_NOT_UPDATED = "Sorry, coordinator hasn't updated";
-    private static final String COURSE_NOT_UPDATED = "Sorry, course hasn't updated";
-    private static final String STUDENT_NOT_UPDATED = "Sorry, student hasn't updated";
+    private static final String COORDINATOR_NOT_FOUND = "Coordinator not found: ";
+    private static final String STUDENT_NOT_FOUND = "Student not found: ";
+    private static final String COORDINATORS_NOT_FOUND = "Coordinators not found: ";
+    private static final String COURSES_NOT_FOUND = "Courses not found: ";
+    private static final String COORDINATOR_NOT_CREATED = "Coordinator not created: ";
+    private static final String COURSE_NOT_CREATED = "Course not created: ";
+    private static final String STUDENT_NOT_CREATED = "Student not created: ";
+    private static final String STUDENT_COURSE_NOT_CREATED = "Student course not created: ";
+    private static final String COORDINATOR_NOT_DELETED = "Coordinator not deleted: ";
+    private static final String COURSE_NOT_DELETED = "Course not deleted: ";
+    private static final String STUDENT_NOT_DELETED = "Student not deleted: ";
+    private static final String STUDENT_COURSE_NOT_DELETED = "Student's course not deleted: ";
+    private static final String COORDINATOR_NOT_UPDATED = "Coordinator not updated: ";
+    private static final String COURSE_NOT_UPDATED = "Course not updated: ";
+    private static final String STUDENT_NOT_UPDATED = "Student not updated: ";
     private static final String COURSE_CREATED_SUCCESS_JSON = "{ \"course_id\" : \"%d\" }";
     private static final String COORDINATOR_CREATED_SUCCESS_JSON = "{ \"coordinator_id\" : \"%d\" }";
     private static final String STUDENT_CREATED_SUCCESS_JSON = "{ \"student_id\" : \"%d\" }";
@@ -42,16 +42,11 @@ public class RestApiServlet extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
-        final Object getRestHandler = getServletContext().getAttribute("getRestHandler");
-        final Object postRestHandler = getServletContext().getAttribute("postRestHandler");
-        final Object putRestHandler = getServletContext().getAttribute("putRestHandler");
-        final Object deleteRestHandler = getServletContext().getAttribute("deleteRestHandler");
 
-
-        this.getRestHandler = (GetRestHandler) getRestHandler;
-        this.postRestHandler = (PostRestHandler) postRestHandler;
-        this.putRestHandler = (PutRestHandler) putRestHandler;
-        this.deleteRestHandler = (DeleteRestHandler) deleteRestHandler;
+        this.getRestHandler = (RestApiHandler) getServletContext().getAttribute("getRestHandler");
+        this.postRestHandler = (RestApiHandler) getServletContext().getAttribute("postRestHandler");
+        this.putRestHandler = (RestApiHandler) getServletContext().getAttribute("putRestHandler");
+        this.deleteRestHandler = (RestApiHandler) getServletContext().getAttribute("deleteRestHandler");
     }
 
 
@@ -60,11 +55,10 @@ public class RestApiServlet extends HttpServlet {
         log.info("Servlet catch request method: [{}] with URI: [{}]", req.getMethod(), req.getRequestURI());
         String pathInfo = req.getPathInfo();
         req.setCharacterEncoding("UTF-8");
-        resp.setContentType("text/HTML; charset=UTF-8");
+        resp.setContentType("application/json; charset=UTF-8");
 
         try {
             String user_response = getRestHandler.handleRestRequest(pathInfo).orElseThrow(SQLException::new);
-            resp.setContentType("application/json; charset=UTF-8");
             resp.setStatus(200);
             PrintWriter out = resp.getWriter();
             out.write(user_response);
@@ -73,21 +67,21 @@ public class RestApiServlet extends HttpServlet {
             log.error("SQLException [{}] with request method: [{}] with URI: [{}] ",
                     e.getMessage(), req.getMethod(), req.getRequestURI());
             PrintWriter out = resp.getWriter();
-            if (pathInfo.contains("coordinator")) {
+            if (pathInfo.contains("coordinators")) {
+                resp.getWriter().write(COORDINATORS_NOT_FOUND + e.getMessage());
+            } else if (pathInfo.contains("courses")) {
+                resp.getWriter().write(COURSES_NOT_FOUND + e.getMessage());
+            }else  if (pathInfo.contains("coordinator")) {
                 resp.getWriter().write(COORDINATOR_NOT_FOUND + e.getMessage());
             } else if (pathInfo.contains("student")) {
                 resp.getWriter().write(STUDENT_NOT_FOUND + e.getMessage());
-            } else if (pathInfo.contains("coordinators/")) {
-                resp.getWriter().write(COORDINATORS_NOT_FOUND + e.getMessage());
-            } else if (pathInfo.contains("courses/")) {
-                resp.getWriter().write(COURSES_NOT_FOUND + e.getMessage());
             }
             resp.setStatus(404);
 
         } catch (JsonProcessingException e) {
             log.error("JsonProcessingException [{}] with request method: [{}] with URI: [{}] ",
                     e.getMessage(), req.getMethod(), req.getRequestURI());
-
+            resp.setStatus(500);
         }
 
 
@@ -98,32 +92,32 @@ public class RestApiServlet extends HttpServlet {
         log.info("Servlet catch request method: [{}] with URI: [{}]", req.getMethod(), req.getRequestURI());
         String pathInfo = req.getPathInfo();
         req.setCharacterEncoding("UTF-8");
-
+        resp.setContentType("application/json; charset=UTF-8");
         try {
             int generated_id = postRestHandler.handleRestRequest(pathInfo, req);
-            resp.setContentType("application/json; charset=UTF-8");
+
             if (pathInfo.contains("coordinator")) {
                 resp.getWriter().write(String.format(COORDINATOR_CREATED_SUCCESS_JSON, generated_id));
-            } else if (pathInfo.contains("course")) {
-                resp.getWriter().write(String.format(COURSE_CREATED_SUCCESS_JSON, generated_id));
-            } else if (pathInfo.contains("student")) {
-                resp.getWriter().write(String.format(STUDENT_CREATED_SUCCESS_JSON, generated_id));
             } else if (pathInfo.contains("student_course")) {
                 resp.getWriter().write(String.format(STUDENT_COURSE_CREATED_SUCCESS_JSON, generated_id));
+            }else if (pathInfo.contains("course")) {
+                resp.getWriter().write(String.format(COURSE_CREATED_SUCCESS_JSON, generated_id));
+            }  else if (pathInfo.contains("student")) {
+                resp.getWriter().write(String.format(STUDENT_CREATED_SUCCESS_JSON, generated_id));
             }
             resp.setStatus(201);
         } catch (SQLException e) {
             log.error("SQLException [{}] with request method: [{}] with URI: [{}] ",
                     e.getMessage(), req.getMethod(), req.getRequestURI());
-            resp.setContentType("text/HTML; charset=UTF-8");
+
             if (pathInfo.contains("coordinator")) {
                 resp.getWriter().write(COORDINATOR_NOT_CREATED + e.getMessage());
-            } else if (pathInfo.contains("course")) {
+            } else if (pathInfo.contains("student_course")) {
+                resp.getWriter().write(STUDENT_COURSE_NOT_CREATED + e.getMessage());
+            }else if (pathInfo.contains("course")) {
                 resp.getWriter().write(COURSE_NOT_CREATED + e.getMessage());
             } else if (pathInfo.contains("student")) {
                 resp.getWriter().write(STUDENT_NOT_CREATED + e.getMessage());
-            } else if (pathInfo.contains("student_course")) {
-                resp.getWriter().write(STUDENT_COURSE_NOT_CREATED + e.getMessage());
             }
             resp.setStatus(400);
         }
@@ -135,24 +129,24 @@ public class RestApiServlet extends HttpServlet {
         log.info("Servlet catch request method: [{}] with URI: [{}]", req.getMethod(), req.getRequestURI());
         String pathInfo = req.getPathInfo();
         req.setCharacterEncoding("UTF-8");
-        resp.setContentType("text/HTML; charset=UTF-8");
+        resp.setContentType("application/json; charset=UTF-8");
         try {
             int deleted_rows = deleteRestHandler.handleRestRequest(pathInfo, req);
             if (deleted_rows != 0) {
                 resp.setStatus(200);
             } else {
-                throw new SQLException();
+                throw new SQLException("Something wrong");
             }
         } catch (SQLException e) {
             log.error("SQLException [{}] with request method: [{}] with URI: [{}] ",
                     e.getMessage(), req.getMethod(), req.getRequestURI());
             resp.setStatus(400);
             if (pathInfo.contains("coordinator")) {
-                resp.getWriter().write(COORDINATOR_NOT_DELETED);
+                resp.getWriter().write(COORDINATOR_NOT_DELETED + e.getMessage());
             } else if (pathInfo.contains("course")) {
-                resp.getWriter().write(COURSE_NOT_DELETED);
+                resp.getWriter().write(COURSE_NOT_DELETED + e.getMessage());
             } else if (pathInfo.contains("student")) {
-                resp.getWriter().write(STUDENT_NOT_DELETED);
+                resp.getWriter().write(STUDENT_NOT_DELETED + e.getMessage());
             }
         }
     }
@@ -162,21 +156,25 @@ public class RestApiServlet extends HttpServlet {
         log.info("Servlet catch request method: [{}] with URI: [{}]", req.getMethod(), req.getRequestURI());
         String pathInfo = req.getPathInfo();
         req.setCharacterEncoding("UTF-8");
-        resp.setContentType("text/HTML; charset=UTF-8");
+        resp.setContentType("application/json; charset=UTF-8");
 
         try {
-            putRestHandler.handleRestRequest(pathInfo, req);
-            resp.setStatus(200);
+           int updated_rows = putRestHandler.handleRestRequest(pathInfo, req);
+            if (updated_rows != 0) {
+                resp.setStatus(200);
+            } else {
+                throw new SQLException("Something wrong");
+            }
         } catch (SQLException e) {
             log.error("SQLException [{}] with request method: [{}] with URI: [{}] ",
                     e.getMessage(), req.getMethod(), req.getRequestURI());
             resp.setStatus(400);
             if (pathInfo.contains("coordinator")) {
-                resp.getWriter().write(COORDINATOR_NOT_UPDATED);
+                resp.getWriter().write(COORDINATOR_NOT_UPDATED + e.getMessage());
             } else if (pathInfo.contains("course")) {
-                resp.getWriter().write(COURSE_NOT_UPDATED);
+                resp.getWriter().write(COURSE_NOT_UPDATED + e.getMessage());
             } else if (pathInfo.contains("student")) {
-                resp.getWriter().write(STUDENT_NOT_UPDATED);
+                resp.getWriter().write(STUDENT_NOT_UPDATED + e.getMessage());
             }
         }
     }
---
+++ src/test/java/com/aston/secondTask/servlets/RestApiServletTest.java
@@ -0,0 +1,325 @@
+package com.aston.secondTask.servlets;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+class RestApiServletTest {
+
+    @InjectMocks
+    private RestApiServlet restApiServlet;
+
+    @Mock
+    private RestApiHandler getRestHandler;
+    @Mock
+    private RestApiHandler postRestHandler;
+    @Mock
+    private RestApiHandler putRestHandler;
+    @Mock
+    private RestApiHandler deleteRestHandler;
+
+    @Mock
+    private HttpServletRequest request;
+    @Mock
+    private HttpServletResponse response;
+    @Mock
+    private PrintWriter writer;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        MockitoAnnotations.openMocks(this);
+        when(response.getWriter()).thenReturn(writer);
+
+        ServletContext servletContext = mock(ServletContext.class);
+        when(servletContext.getAttribute("getRestHandler")).thenReturn(getRestHandler);
+        when(servletContext.getAttribute("postRestHandler")).thenReturn(postRestHandler);
+        when(servletContext.getAttribute("putRestHandler")).thenReturn(putRestHandler);
+        when(servletContext.getAttribute("deleteRestHandler")).thenReturn(deleteRestHandler);
+
+        restApiServlet = new RestApiServlet() {
+            @Override
+            public ServletContext getServletContext() {
+                return servletContext;
+            }
+        };
+        restApiServlet.init();
+    }
+
+    @Test
+    public void testDoGet_Success() throws Exception {
+        String pathInfo = "/somePath";
+        when(request.getPathInfo()).thenReturn(pathInfo);
+        when(getRestHandler.handleRestRequest(pathInfo)).thenReturn(Optional.of("Success"));
+
+        restApiServlet.doGet(request, response);
+
+        verify(response).setContentType("application/json; charset=UTF-8");
+        verify(response).setStatus(200);
+        verify(writer).write("Success");
+    }
+
+    @Test
+    public void testDoGet_Coordinator_SQLException() throws Exception {
+        String pathInfo = "/coordinator";
+        when(request.getPathInfo()).thenReturn(pathInfo);
+        when(getRestHandler.handleRestRequest(pathInfo)).thenThrow(new SQLException("Database error"));
+
+        restApiServlet.doGet(request, response);
+
+        verify(response).setStatus(404);
+        verify(writer).write("Coordinator not found: Database error");
+    }
+
+    @Test
+    public void testDoGet_Student_SQLException() throws Exception {
+        String pathInfo = "/student";
+        when(request.getPathInfo()).thenReturn(pathInfo);
+        when(getRestHandler.handleRestRequest(pathInfo)).thenThrow(new SQLException("Database error"));
+
+        restApiServlet.doGet(request, response);
+
+        verify(response).setStatus(404);
+        verify(writer).write("Student not found: Database error");
+    }
+
+    @Test
+    public void testDoGet_Courses_SQLException() throws Exception {
+        String pathInfo = "/courses";
+        when(request.getPathInfo()).thenReturn(pathInfo);
+        when(getRestHandler.handleRestRequest(pathInfo)).thenThrow(new SQLException("Database error"));
+
+        restApiServlet.doGet(request, response);
+
+        verify(response).setStatus(404);
+        verify(writer).write("Courses not found: Database error");
+    }
+
+    @Test
+    public void testDoGet_Coordinators_SQLException() throws Exception {
+        String pathInfo = "/coordinators";
+        when(request.getPathInfo()).thenReturn(pathInfo);
+        when(getRestHandler.handleRestRequest(pathInfo)).thenThrow(new SQLException("Database error"));
+
+        restApiServlet.doGet(request, response);
+
+        verify(response).setStatus(404);
+        verify(writer).write("Coordinators not found: Database error");
+    }
+
+    @Test
+    public void testDoGet_JsonProcessingException() throws Exception {
+        String pathInfo = "/somePath";
+        when(request.getPathInfo()).thenReturn(pathInfo);
+        when(getRestHandler.handleRestRequest(pathInfo)).thenThrow(new JsonProcessingException("JSON error") {
+        });
+
+        restApiServlet.doGet(request, response);
+
+        verify(response).setStatus(500);
+        verify(writer, never()).write(anyString());
+    }
+
+    @Test
+    public void testDoPost_Coordinator_Success() throws Exception {
+        String pathInfo = "/coordinator";
+        int generatedId = 1;
+        when(request.getPathInfo()).thenReturn(pathInfo);
+        when(postRestHandler.handleRestRequest(pathInfo, request)).thenReturn(generatedId);
+
+        restApiServlet.doPost(request, response);
+
+        verify(response).setContentType("application/json; charset=UTF-8");
+        verify(response).setStatus(201);
+        verify(writer).write("{ \"coordinator_id\" : \"1\" }");
+    }
+
+    @Test
+    public void testDoPost_Course_Success() throws Exception {
+        String pathInfo = "/course";
+        int generatedId = 2;
+        when(request.getPathInfo()).thenReturn(pathInfo);
+        when(postRestHandler.handleRestRequest(pathInfo, request)).thenReturn(generatedId);
+
+        restApiServlet.doPost(request, response);
+
+        verify(response).setContentType("application/json; charset=UTF-8");
+        verify(response).setStatus(201);
+        verify(writer).write("{ \"course_id\" : \"2\" }");
+    }
+
+    @Test
+    public void testDoPost_Student_Success() throws Exception {
+        String pathInfo = "/student";
+        int generatedId = 1;
+        when(request.getPathInfo()).thenReturn(pathInfo);
+        when(postRestHandler.handleRestRequest(pathInfo, request)).thenReturn(generatedId);
+
+        restApiServlet.doPost(request, response);
+
+        verify(response).setContentType("application/json; charset=UTF-8");
+        verify(response).setStatus(201);
+        verify(writer).write("{ \"student_id\" : \"1\" }");
+    }
+
+    @Test
+    public void testDoPost_Student_Course_Success() throws Exception {
+        String pathInfo = "/student_course";
+        int generatedId = 2;
+        when(request.getPathInfo()).thenReturn(pathInfo);
+        when(postRestHandler.handleRestRequest(pathInfo, request)).thenReturn(generatedId);
+
+        restApiServlet.doPost(request, response);
+
+        verify(response).setContentType("application/json; charset=UTF-8");
+        verify(response).setStatus(201);
+        verify(writer).write("{ \"student_course_id\" : \"2\" }");
+    }
+
+    @Test
+    public void testDoPost_Course_SQLException() throws Exception {
+        String pathInfo = "/course";
+        when(request.getPathInfo()).thenReturn(pathInfo);
+        when(postRestHandler.handleRestRequest(pathInfo, request)).thenThrow(new SQLException("Database error"));
+
+        restApiServlet.doPost(request, response);
+
+        verify(response).setStatus(400);
+        verify(writer).write("Course not created: Database error");
+    }
+
+    @Test
+    public void testDoPost_Student_SQLException() throws Exception {
+        String pathInfo = "/student";
+        when(request.getPathInfo()).thenReturn(pathInfo);
+        when(postRestHandler.handleRestRequest(pathInfo, request)).thenThrow(new SQLException("Database error"));
+
+        restApiServlet.doPost(request, response);
+
+        verify(response).setStatus(400);
+        verify(writer).write("Student not created: Database error");
+    }
+
+    @Test
+    public void testDoPost_Coordinator_SQLException() throws Exception {
+        String pathInfo = "/coordinator";
+        when(request.getPathInfo()).thenReturn(pathInfo);
+        when(postRestHandler.handleRestRequest(pathInfo, request)).thenThrow(new SQLException("Database error"));
+
+        restApiServlet.doPost(request, response);
+
+        verify(response).setStatus(400);
+        verify(writer).write("Coordinator not created: Database error");
+    }
+
+    @Test
+    public void testDoPost_Student_Course_SQLException() throws Exception {
+        String pathInfo = "/student_course";
+        when(request.getPathInfo()).thenReturn(pathInfo);
+        when(postRestHandler.handleRestRequest(pathInfo, request)).thenThrow(new SQLException("Database error"));
+
+        restApiServlet.doPost(request, response);
+
+        verify(response).setStatus(400);
+        verify(writer).write("Student course not created: Database error");
+    }
+
+    @Test
+    public void testDoDelete_Successful() throws Exception {
+        String pathInfo = "/somePath";
+        int numberRows = 2;
+        when(request.getPathInfo()).thenReturn(pathInfo);
+        when(deleteRestHandler.handleRestRequest(pathInfo, request)).thenReturn(numberRows);
+
+        restApiServlet.doDelete(request, response);
+
+        verify(response).setStatus(200);
+    }
+
+    @Test
+    public void testDoDelete_Not_Successful() throws Exception {
+        String pathInfo = "/coordinator";
+        int numberRows = 0;
+        when(request.getPathInfo()).thenReturn(pathInfo);
+        when(deleteRestHandler.handleRestRequest(pathInfo, request)).thenReturn(numberRows);
+
+        restApiServlet.doDelete(request, response);
+
+        verify(response).setStatus(400);
+        verify(writer).write("Coordinator not deleted: Something wrong");
+    }
+
+    @Test
+    public void testDoDelete_Course_SQLException() throws Exception {
+        String pathInfo = "/course";
+        when(request.getPathInfo()).thenReturn(pathInfo);
+        when(deleteRestHandler.handleRestRequest(pathInfo, request)).thenThrow(new SQLException("Database error"));
+
+        restApiServlet.doDelete(request, response);
+
+        verify(response).setStatus(400);
+        verify(writer).write("Course not deleted: Database error");
+    }
+
+    @Test
+    public void testDoPut_Successful() throws Exception {
+        String pathInfo = "/somePath";
+        int updatedRows = 1;
+        when(request.getPathInfo()).thenReturn(pathInfo);
+        when(putRestHandler.handleRestRequest(pathInfo, request)).thenReturn(1);
+
+        restApiServlet.doPut(request, response);
+
+        verify(response).setContentType("application/json; charset=UTF-8");
+        verify(response).setStatus(200);
+    }
+    @Test
+    public void testDoPut_Not_Successful() throws Exception {
+        String pathInfo = "/coordinator";
+        int numberRows = 0;
+        when(request.getPathInfo()).thenReturn(pathInfo);
+        when(putRestHandler.handleRestRequest(pathInfo, request)).thenReturn(numberRows);
+
+        restApiServlet.doPut(request, response);
+
+        verify(response).setStatus(400);
+        verify(writer).write("Coordinator not updated: Something wrong");
+    }
+    @Test
+    public void testDoPut_Course_SQLException() throws Exception {
+        String pathInfo = "/course";
+        when(request.getPathInfo()).thenReturn(pathInfo);
+        when(putRestHandler.handleRestRequest(pathInfo, request)).thenThrow(new SQLException("Database error"));
+
+        restApiServlet.doPut(request, response);
+
+        verify(response).setStatus(400);
+        verify(writer).write("Course not updated: Database error");
+    }
+    @Test
+    public void testDoPut_Student_SQLException() throws Exception {
+        String pathInfo = "/student";
+        when(request.getPathInfo()).thenReturn(pathInfo);
+        when(putRestHandler.handleRestRequest(pathInfo, request)).thenThrow(new SQLException("Database error"));
+
+        restApiServlet.doPut(request, response);
+
+        verify(response).setStatus(400);
+        verify(writer).write("Student not updated: Database error");
+    }
+
+
+}
